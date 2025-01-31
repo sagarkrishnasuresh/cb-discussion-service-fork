ad271deaecf3bd8780425ef61f7b35fa193fc7c3
ruksanasemeir <ruksana.semeir@tarento.com>
2025-01-31 14:29:50 +0530

    Added the code to update the post count of community

--- src/main/java/com/igot/cb/discussion/service/impl/DiscussionServiceImpl.java
+++ src/main/java/com/igot/cb/discussion/service/impl/DiscussionServiceImpl.java
@@ -84,7 +84,7 @@ public class DiscussionServiceImpl implements DiscussionService {
     @Value("${kafka.topic.community.post.count}")
     private String communityPostCount;
 
-//    @PostConstruct
+    @PostConstruct
     public void init() {
         if (storageService == null) {
             storageService = StorageServiceFactory.getStorageService(new StorageConfig(cbServerProperties.getCloudStorageTypeName(), cbServerProperties.getCloudStorageKey(), cbServerProperties.getCloudStorageSecret().replace("\\n", "\n"), Option.apply(cbServerProperties.getCloudStorageEndpoint()), Option.empty()));
