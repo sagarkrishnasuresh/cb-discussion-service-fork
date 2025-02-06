f0761cc53392ea255ccec03be9de11fbb8115da7
ruksanasemeir <ruksana.semeir@tarento.com>
2025-02-06 16:59:41 +0530

    Issue to solve kafka issue

--- src/main/java/com/igot/cb/config/ConsumerConfiguration.java
+++ src/main/java/com/igot/cb/config/ConsumerConfiguration.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -13,6 +14,7 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class ConsumerConfiguration {
 
     @Value("${spring.kafka.bootstrap.servers}")
