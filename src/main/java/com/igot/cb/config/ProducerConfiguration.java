f632129dd305de65e2d6e274ec95a00e002ebd06
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-02-06 23:00:25 +0530

    reverted unused code

--- src/main/java/com/igot/cb/config/ProducerConfiguration.java
+++ src/main/java/com/igot/cb/config/ProducerConfiguration.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -12,7 +11,6 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
 public class ProducerConfiguration {
 
     @Value("${spring.kafka.bootstrap.servers}")
