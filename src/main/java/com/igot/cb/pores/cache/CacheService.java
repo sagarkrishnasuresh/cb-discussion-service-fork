38af7f5b705de5b55dd7c200f2029745a54f9fef
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-05-15 13:26:21 +0530

    resolved redis issue

--- src/main/java/com/igot/cb/pores/cache/CacheService.java
+++ src/main/java/com/igot/cb/pores/cache/CacheService.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.igot.cb.pores.util.Constants;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @Service
 @Slf4j
 public class CacheService {
+
+  @Autowired
+  private JedisPool jedisPool;
   @Autowired
   private ObjectMapper objectMapper;
 
@@ -21,42 +24,55 @@ public class CacheService {
   private long cacheTtl;
 
   @Autowired
-  private RedisTemplate<String, String> redisTemplate;
+  private JedisPool jedisDataPopulationPool;
 
-  public String getCache(String key) {
-    try {
-      return redisTemplate.opsForValue().get(Constants.REDIS_KEY_PREFIX + key);
-    } catch (Exception e) {
-      log.error("Error while getting data from Redis cache: {} ", e.getMessage());
-      return null;
+  public Jedis getJedis() {
+    try (Jedis jedis = jedisPool.getResource()) {
+      return jedis;
     }
   }
 
   public void putCache(String key, Object object) {
     try {
       String data = objectMapper.writeValueAsString(object);
-      redisTemplate.opsForValue().set(Constants.REDIS_KEY_PREFIX + key, data, cacheTtl, TimeUnit.SECONDS);
+      try (Jedis jedis = jedisPool.getResource()) {
+        jedis.set(Constants.REDIS_KEY_PREFIX + key, data);
+        jedis.expire(Constants.REDIS_KEY_PREFIX + key, cacheTtl);
+      }
     } catch (Exception e) {
       log.error("Error while putting data in Redis cache: {} ", e.getMessage());
     }
   }
 
-  public Long deleteCache(String key) {
-    boolean result = redisTemplate.delete(Constants.REDIS_KEY_PREFIX + key);
-    if (result) {
-      log.info("Field deleted successfully from key {}.", key);
-    } else {
-      log.warn("Field not found in key {}.", key);
+  public String getCache(String key) {
+    try {
+      return getJedis().get(Constants.REDIS_KEY_PREFIX + key);
+    } catch (Exception e) {
+      return null;
     }
-    return null;
+  }
+
+  public Long deleteCache(String key) {
+    try (Jedis jedis = jedisPool.getResource()) {
+      Long result = jedis.del(Constants.REDIS_KEY_PREFIX + key);
+      if (result == 1) {
+        log.info("Field {} deleted successfully from key {}.", key);
+      } else {
+        log.warn("Field {} not found in key {}.", key);
+      }
+      return result;
+    } catch (Exception e) {
+      log.error("Error while deleting data from Redis cache: {} ", e.getMessage());
+      return null;
+    }
   }
 
   public List<Object> hget(List<String> keys) {
     List<Object> resultList = new ArrayList<>();
-    try {
+    try (Jedis jedis = jedisDataPopulationPool.getResource()) {
       for (String key : keys) {
-        String value = redisTemplate.opsForValue().get(key);
-        resultList.add(value);
+        String values = jedis.get(key); // Fetch entire hash
+        resultList.add(values); // Add map as an Object
       }
     } catch (Exception e) {
       log.error("Error while fetching data from Redis: {}", e.getMessage(), e);
